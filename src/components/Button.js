219de8325ab3bef3cd5dd91ab6ede8cda5b3d065
Rain Zhu <rain1995zhu@gmail.com>
2023-04-19 22:51:02 -0700

    add button with classNames Library

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+
+  const buttonClass = classNames("button", { 
+    " button--danger": props.danger,
+    " button--confirm": props.confirm 
+   });
+
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className={buttonClass}
+    >
+      {props.children}
+    </button>);
 }
