93edf1d883958d5ab1a63029d055e22951b05fa6
Rain Zhu <rain1995zhu@gmail.com>
2023-04-20 23:44:44 -0700

    add appointment and empty components

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,7 +11,7 @@ export default function InterviewList(props) {
       name={interviewer.name}
       avatar={interviewer.avatar}
       selected={interviewer.id === props.value}
-      setInterviewer={() => onChange(interviewer.id)}
+      setInterviewer={() => props.onChange(interviewer.id)}
       />
     );
   });
