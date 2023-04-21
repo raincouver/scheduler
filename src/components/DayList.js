452d2043aaa072b07626c94f9debd1660dd1f12b
Rain Zhu <rain1995zhu@gmail.com>
2023-04-20 20:13:51 -0700

    add dayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,24 @@
 import React from "react";
-import classNames from "classnames";
-import "components/DayListItem.js";
+import DayListItem from "components/DayListItem";
 
 
 export default function DayList(props) {
 
+  const daysProps = props.days.map((day) => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
   return (
     <ul>
+      {daysProps}
     </ul>
   );
 }
